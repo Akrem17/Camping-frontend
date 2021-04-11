9d9993954ae5f00930cfc2fdf20972ce3f43df74
Akrem Hammami <akrem@gmx.us>
2021-04-11 14:40:44 +0200

    add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+<<<<<<< Updated upstream
 
 
 const routes: Routes = [];
+=======
+import { LoginComponent } from './login/login.component';
+import { ToursOverViewComponent } from './tours-over-view/tours-over-view.component';
+
+
+const routes: Routes = [
+  {path: 'tours', component: ToursOverViewComponent, },
+  {path: '', redirectTo:'tours', pathMatch: 'full'},
+  {path: 'login',component:LoginComponent},
+];
+>>>>>>> Stashed changes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+<<<<<<< Updated upstream
 
+=======
+import { LoginComponent } from './login/login.component';
+>>>>>>> Stashed changes
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+<<<<<<< Updated upstream
     FooterComponent
+=======
+    FooterComponent,
+    LoginComponent,
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
