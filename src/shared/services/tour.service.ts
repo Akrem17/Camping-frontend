05eb919a346c35d1babb14b0bb37501db74942b3
unknown <rabbegmouhib@gmail.com>
2021-05-18 12:23:14 +0100

    add get tours and get tour services

--- src/shared/services/tour.service.ts
+++ src/shared/services/tour.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Tour } from 'src/models/tour.modal';
+import { environment} from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,10 @@ export class TourService {
   constructor(private http:HttpClient) { }
 
   getTours(){
-    //call the api 
+    return this.http.get(`${environment.url}/randonnee/get_randonnee`)
+  }
+
+  getTour(id:string){
+    return this.http.get(`${environment.url}/randonnee/get_randonnee/${id}`)
   }
 }
