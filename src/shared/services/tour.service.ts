f71ca067a586804fab05a30aba117a7a5dabc9cb
unknown <rabbegmouhib@gmail.com>
2021-05-24 19:00:58 +0100

    fix search result using routing

--- src/shared/services/tour.service.ts
+++ src/shared/services/tour.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Tour } from 'src/models/tour.modal';
+
 import { environment} from '../../environments/environment'
 
 @Injectable({
@@ -8,10 +8,22 @@ import { environment} from '../../environments/environment'
 })
 export class TourService {
 
+ 
   constructor(private http:HttpClient) { }
 
-  getTours(){
-    return this.http.get(`${environment.url}/randonnee/get_randonnee`)
+
+  
+
+
+
+  getTours(search?){
+    
+    if(search){
+      return this.http.get(`${environment.url}/randonnee/get_randonnee?startLocation=${search}`)
+    }else{
+      return this.http.get(`${environment.url}/randonnee/get_randonnee`)
+    }
+     
   }
 
   getTour(id:string){
